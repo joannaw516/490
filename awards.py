418c3594dad22ecc572a5ed8f9920352f815d143
joannaw516 <joanna.j.wu@yale.edu>
2020-04-19 23:20:43 -0400

    comments

--- awards.py
+++ awards.py
@@ -1,22 +1,28 @@
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
+#import pandas for exporting to csv, requests for fetching html, beautifulsoup for parsing html
 
+#using requests to get html from url, and beautiful soup to parse html content
 page = requests.get('https://www.societyforscience.org/regeneron-sts/2020-finalists/')
 
 soup = BeautifulSoup(page.content, 'html.parser')
 
+#finding main content by id to have access 
 table = soup.find(id= 'content')
 
+#get the students names
 names = table.find_all('strong')
 
+#get the projects
 projects = table.find_all('p')
 
+#strip just the text
 names = [item.get_text() for item in names]
 
 projects = [item.get_text() for item in projects]
 
-
+#create csv
 awards = pd.DataFrame({
 	'names:' : names,
 	'projects:' : projects[1:],
--- awardscholars.py
+++ awardscholars.py
@@ -1,34 +1,38 @@
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
+#import pandas for exporting to csv, requests for fetching html, beautifulsoup for parsing html
 
+#using requests to get html from url, and beautiful soup to parse html content
 page = requests.get('https://www.societyforscience.org/regeneron-sts/2020-scholars/')
 
 soup = BeautifulSoup(page.content, 'html.parser')
 
+#finding main content by id to have access 
 table = soup.find(id= 'content')
 
+#get student names
 names = table.find_all('strong')
-
+#get student projects
 projects = table.find_all('p')
 
 newnames = []
 
-
+#there were some empty spots that were also classified as 'strong', had to get rid of those
 for item in range(0, len(names)):
 	if(names[item].get_text() != '\xa0'):
 		newnames.append(names[item].get_text())
-	print(names[item], item)
+	#print(names[item], item)
 	
 
 #names = [item.get_text() for item in names]
+
+#did the same thing for the projects
 finalprojects = []
 for item in range(0, len(projects)):
 	if(projects[item].get_text() != '\xa0'):
 		finalprojects.append(projects[item].get_text())
 	
-#projects = [item.get_text() for item in projects]
-print(len(finalprojects))
 
 awards = pd.DataFrame({
 	'names:' : newnames,
